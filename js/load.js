32aa4f1babf75464b54d5aaa7354f82e760c8de0
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2019-10-16 23:53:22 +0300

    данные с сервера
    вывела реальные пины из полученных данных

---
+++ js/load.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+
+  window.mainBlock = document.querySelector('.main');
+  window.errorTpl = document.querySelector('#error');
+
+  // подгрузка данных с сервера
+
+window.load = function (onSuccess, onError, url) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    xhr.open('GET', url);
+    xhr.send();
+  };
+
+})();
--- js/pins.js
+++ js/pins.js
@@ -2,70 +2,18 @@
 
 (function () {
 
-  var TITLES = ['Offer 1', 'Offer 2', 'Offer 3', 'Offer 4', 'Offer 5', 'Offer 6', 'Offer 7', 'Offer 8'];
-  var DESCRIPTIONS = ['descrpition 1', 'descrpition 2', 'descrpition 3', 'descrpition 4', 'descrpition 5', 'descrpition 6', 'descrpition 7', 'descrpition 8'];
-  var TIMECHECKS = ['12:00', '13:00', '14:00'];
-  var HOUSES = ['palace', 'flat', 'house', 'bungalo'];
-  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var PICS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+  var AVATAR_SIZE = 40;
   var PIN_WIDTH = 50;
   var PIN_HEIGHT = 70;
-  var AVATAR_SIZE = 40;
-  var MIN_Y = 130;
-  var MAX_Y = 630;
-  var MIN_GUESTS = 1;
-  var MAX_GUESTS = 20;
-  var MIN_ROOMS = 1;
-  var MAX_ROOMS = 6;
-  var MIN_PRICE = 1000;
-  var MAX_PRICE = 8345;
+  var DATA_SRC = 'https://js.dump.academy/keksobooking/data';
 
   var map = document.querySelector('.map');
   var pinsContainer = document.querySelector('.map__pins');
   var mapFilters = document.querySelector('.map__filters');
 
-  // генерируем объект-объявление
-
-  var generateOffer = function (citymapOffsetWidth) {
-    return {
-      author: {avatar: window.getRandomImg(1, 8)},
-      offer: {
-        title: window.chooseRandom(TITLES),
-        address:
-        window.getRandomInRange(0, citymapOffsetWidth) +
-        ', ' +
-        window.getRandomInRange(MIN_Y, MAX_Y),
-        price: window.getRandomInRange(MIN_PRICE, MAX_PRICE),
-        type: window.chooseRandom(HOUSES),
-        rooms: window.getRandomInRange(MIN_ROOMS, MAX_ROOMS),
-        guests: window.getRandomInRange(MIN_GUESTS, MAX_GUESTS),
-        checkin: window.chooseRandom(TIMECHECKS),
-        checkout: window.chooseRandom(TIMECHECKS),
-        features: window.randomSort(FEATURES),
-        description: window.chooseRandom(DESCRIPTIONS),
-        photos: window.randomSort(PICS)
-      },
-      location: {
-        x: window.getRandomInRange(0, citymapOffsetWidth),
-        y: window.getRandomInRange(MIN_Y, MAX_Y)
-      }
-    };
-  };
-
-  // собираем массив из 8-ми таких объектов
-
-  var generateOffersArray = function (n, citymap) {
-    var result = [];
-    var mapWidth = citymap.offsetWidth;
-    for (var i = 0; i < n; i++) {
-      result.push(generateOffer(mapWidth));
-    }
-    return result;
-  };
-
   // создаём dom-элементы меток и отрисовываем их
 
-  var showLocation = function (allOffers, pinsBlock) {
+  var showLocation = function (allOffers) {
     var fragment = document.createDocumentFragment();
 
     // создаём метку
@@ -97,15 +45,23 @@
       // выводим метку в html
 
       fragment.appendChild(pin);
-      pinsBlock.appendChild(fragment);
+      pinsContainer.appendChild(fragment);
     }
-    return pinsBlock;
+    return pinsContainer;
   };
 
   // выводим метки
 
-  var offers = generateOffersArray(8, map);
-  showLocation(offers, pinsContainer);
+  var successHandler = function (allOffers) {
+    showLocation(allOffers);
+    console.log(allOffers);
+      console.log('объявление номер восемь ' + allOffers[8].offer.address);
+  };
+  var errorHandler = function (errorMessage) {
+    console.log('ошибка при загрузке: ' + errorMessage);
+  };
+
+  var offers = window.load(successHandler, errorHandler, DATA_SRC);
 
   // изначально неактивный режим
 
