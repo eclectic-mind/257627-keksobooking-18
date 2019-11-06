bb6daea81b7b3763cf5fad5fcba1beb28247b66b
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2019-11-06 23:22:12 +0300

    ...

--- js/load.js
+++ js/load.js
@@ -53,7 +53,7 @@
 
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
-        onSuccess(xhr.response);
+        onSuccess();
         var data = JSON.parse(this.responseText);
       } else {
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
